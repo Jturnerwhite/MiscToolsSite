a419386637c52bbcd9ffc1f62e0cb6d71c538197
Jordan White <jordan@volanosoftware.com>
2021-09-27 14:34:18 -0500

    more stylin - extra uggo

--- misc-tool-site/src/Components/SideNav/sidenav.js
+++ misc-tool-site/src/Components/SideNav/sidenav.js
@@ -5,7 +5,9 @@ function SideNav() {
     return (
         <>
             <ul className="site-nav">
-                <li><a href="/">Event Generator</a></li>
+                <li className="section-name">Generators</li>
+                <li><a href="/gen/event">Event Generator</a></li>
+                <li className="section-name">Misc Tools</li>
                 <li><a href="/">Other Tool</a></li>
                 <li><a href="/">Other Tool</a></li>
                 <li><a href="/">Other Tool</a></li>
